98d115ebf746fa1ded558f564d48ce3af32d7cca
misnosugianto48 <misnosugianto48@gmail.com>
2024-04-04 16:52:32 +0700

    MP-4042024: merger jquery to document

--- script.js
+++ script.js
@@ -1,20 +1,26 @@
 $(document).ready(function () {
-    $(window).scroll(function () {
-      var aboutSection = $("#about_section");
-      var aboutSectionPos = aboutSection.offset().top;
-      var windowHeight = $(window).height();
-      var scrollPos = $(window).scrollTop();
 
-      if (scrollPos > aboutSectionPos - windowHeight + 200) {
-        aboutSection.addClass("fade-in").removeClass("fade-out");
-      } else {
-        aboutSection.removeClass("fade-in").addClass("fade-out");
-      }
-    });
+  // **About Section Fade-in:**
+
+  var aboutSection = $("#about_section");
+  var aboutSectionPos = aboutSection.offset().top;
+  var windowHeight = $(window).height();
+
+  $(window).scroll(function () {
+    var scrollPos = $(window).scrollTop();
+
+    if (scrollPos > aboutSectionPos - windowHeight + 200) {
+      aboutSection.addClass("fade-in").removeClass("fade-out");
+    } else {
+      aboutSection.removeClass("fade-in").addClass("fade-out");
+    }
   });
 
-$(document).ready(function () {
-  $(".pricing_item").fadeIn(1000);
+  // **Pricing Item Fade-in:**
+
+  $(".pricing_item").each(function () {
+    $(this).fadeIn(0); // Pre-load pricing items hidden (optional)
+  });
 
   $(window).scroll(function () {
     $(".pricing_item").each(function () {
@@ -23,6 +29,8 @@ $(document).ready(function () {
 
       if (position < scroll + $(window).height() - 200) {
         $(this).fadeIn(1000);
+      } else {
+        $(this).fadeOut(0);
       }
     });
   });
